89642295f9ca5359b09b6538808caae8807e76eb
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-15 21:59:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,20 +25,4 @@ def retrieval
 
 end
 
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-shipping_manifest["muskets"]=> 2
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-shipping_manifest["gun powder"] => 4
-
-  # return the shipping_manifest hash below
-shipping_manifest
-end
